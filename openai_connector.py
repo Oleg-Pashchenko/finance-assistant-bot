e85610c5484155dbbca66d42d582a0a335982d21
olehandro <odpash.itmo@gmail.com>
2023-10-17 12:47:31 +0300

    Added reqs

--- openai_connector.py
+++ openai_connector.py
@@ -46,7 +46,7 @@ def detect_variables(text: str, owner_id: int) -> (bool, db.Operation):
             return False, None
         return True, db.Operation(
             owner=owner_id,
-            description=function_args['description'].lower(), amount=function_args['amount'],
+            description=function_args['description'].lower(), amount=abs(function_args['amount']),
             is_income=bool(int(function_args['is_income'])), date=datetime.datetime.now().date())
     else:
         return False, None
